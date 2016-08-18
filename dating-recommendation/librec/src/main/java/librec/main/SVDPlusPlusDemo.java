545ae0590209a0f13e7f57a3f1d0194a6463ee3f
Anh Cao <caodunganh@gmail.com>
2016-08-18 12:01:40 +0700

    Auto run update SVD++

--- dating-recommendation/librec/src/main/java/librec/main/SVDPlusPlusDemo.java
+++ dating-recommendation/librec/src/main/java/librec/main/SVDPlusPlusDemo.java
@@ -24,6 +24,9 @@ import librec.util.FileIO;
 import librec.util.Logs;
 import librec.util.Systems;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 /**
@@ -35,12 +38,55 @@ import java.util.Scanner;
 public class SVDPlusPlusDemo {
 
 	public static void main(String[] args) {
+		String dirPath = FileIO.makeDirPath("demo");
+		String defaultConfigFile = FileIO.makeDirPath(dirPath, "config") + "SVD++_u.conf";
+
+		String content = "dataset.ratings.wins=.\\\\demo\\\\Datasets\\\\e4\\\\%d-u.txt\n" +
+				"dataset.ratings.lins=./demo/Datasets/e4/%d-u.txt\n" +
+				"\n" +
+				"testset.ratings.wins = .\\\\demo\\\\Datasets\\\\e4\\\\test.txt\n" +
+				"testset.ratings.lins = ./demo/Datasets/e4/test.txt\n" +
+				"\n" +
+				"updateset.ratings.wins = .\\\\demo\\\\Datasets\\\\e4\\\\%d-r.txt\n" +
+				"updateset.ratings.lins = ./demo/Datasets/e4/%d-r.txt\n" +
+				"\n" +
+				"fullset.ratings.wins = .\\\\demo\\\\Datasets\\\\e4\\\\%d-t.txt\n" +
+				"fullset.ratings.lins = ./demo/Datasets/e4/%d-t.txt\n" +
+				"\n" +
+				"\n" +
+				"ratings.setup=-columns 0 1 2 -threshold -1\n" +
+				"\n" +
+				"recommender=SVD++\n" +
+				"evaluation.setup=train_test_update -k 5 -p off --rand-seed 1 --test-view all\n" +
+				"item.ranking=off -topN -1 -ignore -1\n" +
+				"\n" +
+				"num.factors=100\n" +
+				"num.max.iter=1000\n" +
+				"\n" +
+				"learn.rate=0.01 -max -1 -bold-driver\n" +
+				"reg.lambda=0.1 -u 0.1 -i 0.1 -b 0.1 -s 0.001\n" +
+				"\n" +
+				"output.setup=on -dir ./demo/Results/";
+
 		try {
 			SVDPlusPlusDemo demo = new SVDPlusPlusDemo();
 			demo.execute(args, "SVD++_init.conf", false);
-
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+
+		for (Integer i = 0; i < 10; i++) {
+			try {
+				System.out.println(String.format(content, i, i, i, i, i, i));
+				PrintWriter writer = new PrintWriter(defaultConfigFile, "UTF-8");
+				writer.println(String.format(content, i, i, i, i, i, i));
+				writer.close();
+
+				SVDPlusPlusDemo demo = new SVDPlusPlusDemo();
+				demo.execute(args, "SVD++_u.conf", true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -56,17 +102,17 @@ public class SVDPlusPlusDemo {
 		Logs.config(dirPath + "log4j.xml", true);
 
         String inputConfigFile;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter config file's location (default '" + defaultConfigFile + "'): ");
-        inputConfigFile = reader.nextLine();
+//        Scanner reader = new Scanner(System.in);
+//        System.out.print("Enter config file's location (default '" + defaultConfigFile + "'): ");
+//        inputConfigFile = reader.nextLine();
 
-        if (inputConfigFile.isEmpty()) {
-            System.out.println("Empty input, using default config from (" + defaultConfigFile + ")");
-            configFile = defaultConfigFile;
-        } else {
-            System.out.println("Using config file from (" + inputConfigFile + ")");
-            configFile = inputConfigFile;
-        }
+//        if (inputConfigFile.isEmpty()) {
+		System.out.println("Empty input, using default config from (" + defaultConfigFile + ")");
+		configFile = defaultConfigFile;
+//        } else {
+//            System.out.println("Using config file from (" + inputConfigFile + ")");
+//            configFile = inputConfigFile;
+//        }
 
 		// run algorithm
 		LibRec librec = new LibRec();
@@ -75,6 +121,6 @@ public class SVDPlusPlusDemo {
 
 		// await next command
 		Logs.debug();
-		Systems.pause();
+//		Systems.pause();
 	}
 }
